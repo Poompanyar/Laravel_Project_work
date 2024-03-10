437282aa1fcc78684aa8296366ad86f8adfc0a50
poompanyar <122159004+Poompanyar@users.noreply.github.com>
2024-03-10 11:50:19 +0700

    Add files via upload

---
+++ w01/bio.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+   
+    <body class="bg-gradient-to-r from-blue-500 to-green-500 flex items-center justify-center h-screen">
+        <div class="text-white text-center">
+            <h1 class="text-4xl font-bold mb-4">ยินดีต้อนรับสู่เว็บไซต์ของเรา</h1>
+            <h1 class="text-2xl font-bold text-gray-800 mb-4">สวัสดี, ฉันชือ [Wisdom Suwanrasamee]</h1>
+            <p class="text-black-600">ฉันรักการเขียนโค้ดและการออกแบบเว็บไซต์!</p>
+            <p class="text-black-600">ติดต่อฉันที่ [6414421037@rbru.ac.th]</p>
+            <p class="text-lg">Than You</p>
+            <a href="/about" class="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold px-4 py-2 rounded-full mt-4 inline-block">Next</a>
+        </div>
+        
+</body>
+</html>
